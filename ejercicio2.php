dcf8053171e0e77840434bd9ad6ba59c52a650ea
CesarZV23005 <zv23005@ues.edu.sv>
2025-09-04 12:13:43 -0600

    edit el el archivo ejercicio1, solicitud de datos

---
+++ ejercicio2.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>CONVERSOR DE TEMPERATURA</h1>
+    <label for="temp">Ingrese el valor a convertir</label>
+    <input type="number" name="temp" step="0.01" required>
+</body>
+</html>
