917147f67c39b781787e112ce0b4c50cfe4b62f2
CesarZV23005 <zv23005@ues.edu.sv>
2025-09-04 12:54:00 -0600

    definicion del arreglo en ej3

---
+++ ejercicio3.php
@@ -0,0 +1,18 @@
+<?php
+$compras = [
+    ["producto" => "pan","precio"=> 1.50,"cantidad"=> 5],
+    ["producto" => "arroz","precio"=> 1.00,"cantidad"=> 5],
+    ["producto" => "leche","precio"=> 2.00,"cantidad"=> 5]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
