d30b91338aa4d013a346d21227b0920cda7441ca
CesarZV23005 <zv23005@ues.edu.sv>
2025-09-04 12:41:02 -0600

    maquetacion, definicion de lista contenedora de los personajes

---
+++ ejercicio1.php
@@ -0,0 +1,20 @@
+<?php
+$personajes = [
+    ["personaje" =>"principe","categoria"=> "villanos"],
+    ["personaje" =>"senora","categoria"=> "villanos"],
+    ["personaje" =>"shrek","categoria"=> "principal"],
+    ["personaje" =>"burro","categoria"=> "principal"],
+    
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
