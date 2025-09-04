01953d618cf1951e0996cd91aa40110b5f5ae652
CesarZV23005 <zv23005@ues.edu.sv>
2025-09-04 12:22:31 -0600

    funcionalidad de selects en ejercicio2

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,5 +1,11 @@
 <?php
-
+#farenheit a c
+# F = C * 9/5 + 32
+#celcius  a F
+# C = (F-32) 5/9
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $a = 
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +16,13 @@
 </head>
 <body>
     <h1>CONVERSOR DE TEMPERATURA</h1>
-    <label for="temp">Ingrese el valor a convertir</label>
+    <label for="temp">Ingrese el valor 1 a convertir</label>
     <input type="number" name="temp" step="0.01" required>
+    <label for="temp2">Ingrese el valor 2 a convertir</label>
+    <input type="number" name="temp2" step="0.01" required>
+    <select name="tipo">
+        <option value="farenheit">FARENHEIT</option>
+        <option value="celcius">CELCIUS</option>
+    </select>
 </body>
 </html>
